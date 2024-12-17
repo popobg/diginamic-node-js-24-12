ac6bd81365476060fbd3c47c688f3c57d46c7ef4
popobg <pauline.bouyssou1@gmail.com>
2024-12-17 15:26:57 +0100

    Last exercises

--- exo/mouli_es6/exercices/13_bonus.js
+++ exo/mouli_es6/exercices/13_bonus.js
@@ -24,33 +24,55 @@ ex: la pomme est rouge et la banane est jaune
 
  */
 
-const countWords
+const countWords = (str) => {
+  if (str === "") {
+      return {};
+  }
 
+  // on pourrait couper en prenant en compte la ponctuation en plus des espaces
+  // à l'aide d'une regex comme /[\s,.'!?]+/
+  const words = str.trim().split(" ");
+
+  return obj = words.reduce((acc, word) => {
+      acc[word] = (acc[word] || 0) + 1;
+      return acc;
+  }, {});
+};
 
 /**
 Exercice : Trier un objet par valeur
 
 Objectif :
-Écrire une fonction qui prend en entrée un objet et qui retourne un 
+Écrire une fonction qui prend en entrée un objet et qui retourne un
 nouvel objet dont les propriétés sont triées en fonction de leurs valeurs, en ordre croissant.
 
 exemple:
 {
-  "pommes": 3,
-  "bananes": 1,
-  "cerises": 8
+"pommes": 3,
+"bananes": 1,
+"cerises": 8
 };
 
--> 
+->
 
 {
-  "bananes": 1,
-  "pommes": 3,
-  "cerises": 8
+"bananes": 1,
+"pommes": 3,
+"cerises": 8
 }
 
 */
 
-const sortObjectByValue
+const sortObjectByValue = (obj) => {
+  // convertir l'objet en tableau pour pouvoir utiliser la méthode sort
+  const arr = Object.entries(obj);
+  arr.sort((a, b) => a[1] - b[1]);
+  return Object.fromEntries(arr);
+};
 
-module.exports = {countWords, sortObjectByValue};
+module.exports = { countWords, sortObjectByValue };
+
+console.log(countWords("la pomme est rouge et la banane est jaune"));
+console.log(countWords(""));
+console.log(countWords("la pomme est La Pomme"));
+console.log(countWords("La pomme, la pomme!"));
